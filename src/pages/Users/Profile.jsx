8530cae585baaca1c75529da22d80023c2c9cd5b
JerickTwO <jerickjjtm774@gmail.com>
2024-09-21 15:47:36 -0500

    feat: user data in profile

--- src/pages/Users/Profile.jsx
+++ src/pages/Users/Profile.jsx
@@ -1,32 +1,50 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Dropdown from '../../components/Dropdown';
 import { setPageTitle } from '../../store/themeConfigSlice';
-import { useEffect } from 'react';
-import IconPencilPaper from '../../components/Icon/IconPencilPaper';
+import userService from '../../api/userService'; // Importa tu servicio de usuarios
 import IconCoffee from '../../components/Icon/IconCoffee';
 import IconCalendar from '../../components/Icon/IconCalendar';
-import IconMapPin from '../../components/Icon/IconMapPin';
 import IconMail from '../../components/Icon/IconMail';
 import IconPhone from '../../components/Icon/IconPhone';
-import IconTwitter from '../../components/Icon/IconTwitter';
-import IconDribbble from '../../components/Icon/IconDribbble';
-import IconGithub from '../../components/Icon/IconGithub';
-import IconShoppingBag from '../../components/Icon/IconShoppingBag';
-import IconTag from '../../components/Icon/IconTag';
-import IconCreditCard from '../../components/Icon/IconCreditCard';
-import IconClock from '../../components/Icon/IconClock';
-import IconHorizontalDots from '../../components/Icon/IconHorizontalDots';
 
 const Profile = () => {
     const dispatch = useDispatch();
+    const [user, setUser] = useState({
+        idUser: '',
+        username: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+    });
+
     useEffect(() => {
         dispatch(setPageTitle('Profile'));
-    });
-    const isRtl = useSelector((state) => state.themeConfig.rtlClass) === 'rtl' ? true : false;
+
+        const fetchUserData = async () => {
+            try {
+                // 1. Obtener el objeto completo desde localStorage
+                const storedUser = JSON.parse(localStorage.getItem('user')); // 'user' es la clave que usas para guardar el objeto
+
+                // 2. Verifica que el objeto esté presente y que tenga el campo username
+                if (storedUser && storedUser.username) {
+                    const username = storedUser.username;
+
+                    // 3. Llama al servicio pasando el username obtenido
+                    const userData = await userService.getUser(username);
+                    setUser(userData); // 4. Actualiza el estado con los datos del usuario
+                } else {
+                    console.error('No user found in localStorage');
+                }
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
+        fetchUserData();
+    }, [dispatch]);
+
     return (
         <div>
-
             <div className="pt-5">
                 <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-5 mb-5">
                     <div className="panel">
@@ -35,54 +53,35 @@ const Profile = () => {
                         </div>
                         <div className="mb-5">
                             <div className="flex flex-col justify-center items-center">
-                                <img src="/assets/images/profile-34.jpeg" alt="img" className="w-24 h-24 rounded-full object-cover  mb-5" />
-                                <p className="font-semibold text-primary text-xl">Jimmy Turner</p>
+                                <img src="/assets/images/profile-34.jpeg" alt="img" className="w-24 h-24 rounded-full object-cover mb-5" />
+                                <p className="font-semibold text-primary text-xl">
+                                    {user.firstName} {user.lastName}
+                                </p>
                             </div>
                             <ul className="mt-5 flex flex-col max-w-[160px] m-auto space-y-4 font-semibold text-white-dark">
                                 <li className="flex items-center gap-2">
                                     <IconCoffee className="shrink-0" />
-                                    Web Developer
+                                    {user.username}
                                 </li>
-                                <li className="flex items-center gap-2">
+                                {/* <li className="flex items-center gap-2">
                                     <IconCalendar className="shrink-0" />
-                                    Jan 20, 1989
-                                </li>
-                                <li className="flex items-center gap-2">
-                                    <IconMapPin className="shrink-0" />
-                                    New York, USA
-                                </li>
+                                </li> */}
                                 <li>
                                     <button className="flex items-center gap-2">
                                         <IconMail className="w-5 h-5 shrink-0" />
-                                        <span className="text-primary truncate">jimmy@gmail.com</span>
+                                        <span className="text-primary truncate">{user.email}</span>
                                     </button>
                                 </li>
-                                <li className="flex items-center gap-2">
+                                {/* <li className="flex items-center gap-2">
                                     <IconPhone />
                                     <span className="whitespace-nowrap" dir="ltr">
-                                        +1 (530) 555-12121
                                     </span>
-                                </li>
-                            </ul>
-                            <ul className="mt-7 flex items-center justify-center gap-2">
-                                <li>
-                                    <button className="btn btn-info flex items-center justify-center rounded-full w-10 h-10 p-0">
-                                        <IconTwitter className="w-5 h-5" />
-                                    </button>
-                                </li>
-                                <li>
-                                    <button className="btn btn-danger flex items-center justify-center rounded-full w-10 h-10 p-0">
-                                        <IconDribbble />
-                                    </button>
-                                </li>
-                                <li>
-                                    <button className="btn btn-dark flex items-center justify-center rounded-full w-10 h-10 p-0">
-                                        <IconGithub />
-                                    </button>
-                                </li>
+                                </li> */}
                             </ul>
                         </div>
                     </div>
+
+                    {/* Parte comentada que puedes habilitar cuando sea necesario */}
                     <div className="panel lg:col-span-2 xl:col-span-3">
                         <div className="mb-5">
                             <h5 className="font-semibold text-lg dark:text-white-light">Task</h5>
@@ -123,7 +122,7 @@ const Profile = () => {
                                             <td>Paso 3</td>
                                             <td>
                                                 <div className="h-1.5 bg-[#ebedf2] dark:bg-dark/40 rounded-full flex w-full">
-                                                    <div className="bg-warning rounded-full  w-[39%]"></div>
+                                                    <div className="bg-warning rounded-full w-[39%]"></div>
                                                 </div>
                                             </td>
                                             <td className="text-danger">39%</td>
@@ -133,7 +132,7 @@ const Profile = () => {
                                             <td>Paso 4</td>
                                             <td>
                                                 <div className="h-1.5 bg-[#ebedf2] dark:bg-dark/40 rounded-full flex w-full">
-                                                    <div className="bg-success rounded-full  w-[78.03%]"></div>
+                                                    <div className="bg-success rounded-full w-[78.03%]"></div>
                                                 </div>
                                             </td>
                                             <td className="text-success">78.03%</td>
@@ -144,7 +143,7 @@ const Profile = () => {
                                             <td>Paso 4</td>
                                             <td>
                                                 <div className="h-1.5 bg-[#ebedf2] dark:bg-dark/40 rounded-full flex w-full">
-                                                    <div className="bg-secondary  rounded-full  w-full"></div>
+                                                    <div className="bg-secondary rounded-full w-full"></div>
                                                 </div>
                                             </td>
                                             <td className="text-success">100%</td>
@@ -154,7 +153,7 @@ const Profile = () => {
                                             <td>Paso 5</td>
                                             <td>
                                                 <div className="h-1.5 bg-[#ebedf2] dark:bg-dark/40 rounded-full flex w-full">
-                                                    <div className="bg-danger rounded-full  w-[19.15%]"></div>
+                                                    <div className="bg-danger rounded-full w-[19.15%]"></div>
                                                 </div>
                                             </td>
                                             <td className="text-danger">19.15%</td>
@@ -176,10 +175,8 @@ const Profile = () => {
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     );
 };
-
 export default Profile;
